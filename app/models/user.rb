0c5509d602eafd746b32d2fd723a543a43bfccd9
Jeremy <jeremy.ongws@gmail.com>
2015-07-16 15:47:18 +0800

    sessions without testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   validates :email, presence:true, length: { maximum: 200 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, presence: true, length: {minimum: 9}
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
