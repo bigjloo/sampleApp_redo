471c157012ca58f8193a6bd324727a53b1645334
JLoo <junzhongloo@gmail.com>
2015-07-17 18:23:21 +0800

    finish user controllers

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'pg'
 gem 'bootstrap-sass',       '3.2.0.0'
 gem 'bcrypt',               '3.1.7'
 gem 'faker',                '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 group :development, :test do
   gem 'byebug',      '3.4.0'
   gem 'web-console', '2.0.0.beta3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar123",
-             password_confirmation: "foobar123")
+             password_confirmation: "foobar123",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
